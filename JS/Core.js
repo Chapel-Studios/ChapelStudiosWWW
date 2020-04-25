ab8f5eb31073fdc87649e05093f195dced32f1e4
Chapel Studios <rev.jake@gmail.com>
2020-04-24 18:54:55 -0700

    UI overhaul - mobile size finished, new (expected) bugs to pc and table sizes

---
+++ JS/Core.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    const body = document.getElementsByTagName("body")[0];
+    document.getElementById('navbar-checkbox').addEventListener('click', function () {
+        if (this.checked) {
+            body.classList.add("lock-scroll");
+        }
+        else {
+            body.classList.remove("lock-scroll");
+        }
+    });
+}());
