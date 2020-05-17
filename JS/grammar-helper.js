038c2bfc528e65db9b63d1cb55f4aafbc70cfa3a
Chapel Studios <rev.jake@gmail.com>
2020-05-17 06:01:09 -0700

    added js grammar tool for getting ordinal number info

---
+++ JS/grammar-helper.js
@@ -0,0 +1,21 @@
+﻿'use strict'
+
+var NSJ = NSJ || {};
+NSJ.GrammerHelper = {
+    GetOrdinalSuffix: (input) => {
+        let result = "th";
+        if (input === 1) {
+            result = "st";
+        }
+        else if (input === 2) {
+            result = "nd";
+        }
+        else if (input === 3) {
+            result = "rd";
+        }
+        return result
+    }
+    , IntToOrdinal: (input) => {
+        return input + this.GetCordialSuffix(input);
+    }
+}
