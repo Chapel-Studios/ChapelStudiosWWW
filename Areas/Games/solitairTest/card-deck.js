fb192347e466d0924144f2751fec02d4c1f314c2
ChapelStudios <rev.jake@gmail.com>
2020-05-24 21:38:50 -0700

    test file for new Klondike Solitar game ... ok, maybe more than a test at this point...

---
+++ Areas/Games/solitairTest/card-deck.js
@@ -0,0 +1,71 @@
+'use strict'
+
+class Card {
+    static _template = document.getElementById("CardTemplate");
+
+    static Create (suit, value) {
+        let displayValue = value;
+        if (value == 1) displayValue = "A";
+        else if (value == 11) displayValue = "J";
+        else if (value == 12) value = "Q";
+        else if (value == 13) displayValue = "K";
+
+        let clone =  document.importNode(this._template.content, true);
+        
+        let card = clone.querySelector(".card");
+        let suitAttr = document.createAttribute("suit");
+        suitAttr.value = suit;
+        card.setAttributeNode(suitAttr);
+        let valAttr = document.createAttribute("card-value");
+        valAttr.value = value;
+        card.setAttributeNode(valAttr);
+        
+        let valDivs = clone.querySelectorAll(".value");
+        valDivs.forEach(div => {
+            div.append(displayValue)
+        });
+        
+        let suits = clone.querySelectorAll(".suit");
+        suits.forEach(div => {
+            div.append(suit)
+        });
+        
+        return clone;
+    }
+}
+
+class Suit {
+    constructor(name, img, isRed = false) {
+        this.name = name;
+        this.img = img;
+        this.available = [...Array(14).keys()].slice(1);
+        this.isRed = isRed;
+    }
+}
+
+class Deck {
+    static Suits = [
+        new Suit("Hearts", "", true),
+        new Suit("Diamonds", "", true),
+        new Suit("Spades", ""),
+        new Suit("Clubs", "")
+    ];
+
+    Cards = [];
+
+    constructor () {
+        let suits = Deck.Suits.slice(0);
+        for (let cc = 0; cc < 52; cc++) {
+            let cSuitIndex = NSJ.Random(suits.length);
+            let cSuit = suits[cSuitIndex];
+            let cValIndex = NSJ.Random(cSuit.available.length);
+
+            this.Cards.push(Card.Create(cSuit.name, cSuit.available[cValIndex]));
+
+            cSuit.available.splice(cValIndex, 1);
+            if (cSuit.available.length == 0) {
+                suits.splice(cSuitIndex, 1);
+            }
+        }
+    }
+}
---
+++ Areas/Games/solitairTest/solitaire.js
@@ -0,0 +1,232 @@
+'use strict'
+
+
+class KlondikeGameBoard {
+    _handTemplate = document.getElementById("HandTemplate");
+    _drawPile = document.querySelector("#DrawPile");
+    _dragBox = document.getElementById("DragBox");
+    _gameboard = document.getElementById("Gameboard");
+    _deck;
+
+    _layout() {
+        function createSection(templateID, newID, addEmpty){
+            let template = document.getElementById(templateID);
+            let clone = document.importNode(template.content, true);
+            if (addEmpty) {
+                let empty = document.importNode(document.getElementById("EmptyCard").content, true);
+                let divs = clone.querySelectorAll("div");
+                divs[divs.length - 1].appendChild(empty);
+            }
+
+            clone.querySelector("*").id = newID;
+
+            return clone;
+        }
+
+        Deck.Suits.forEach(suit => {
+           let newSection = createSection("RunTemplate", suit.name, true);
+           this._gameboard.appendChild(newSection);
+        });
+
+        [...Array(8).keys()].slice(1).forEach(i => {
+            let newSection = createSection("StackTemplate", `Stack${i}`, true);
+            this._gameboard.appendChild(newSection);
+        })
+
+        let stacks = document.querySelectorAll("#Gameboard .stack");
+        // Create Stacks
+        for (let i = 0; i < 7; i++) {
+            for (let n = i; n < 7; n++) {
+                let card = this._deck.Cards.pop();
+                if (i == n) {
+                    card.querySelector(".card").classList.add("show");
+                }
+                else {
+                    card.querySelector(".card").classList.add("back");
+                }
+                NSJ.GetDeepestChild(`#Stack${n + 1} .handle`).appendChild(card);
+            }
+        }
+
+        // Create Deck
+        while (this._deck.Cards.length > 0){
+            let card = this._deck.Cards.pop();
+            card.querySelector(".card").classList.add("back");
+            NSJ.GetDeepestChild("#DrawPile .handle").appendChild(card);
+        }
+    }
+
+    _resetDeck = () => {
+        let cards = document.querySelectorAll("#Hands .card");
+        //const pile = document.getElementById("DrawPile");
+        for (let i = cards.length; i > 0; i-- ){
+            let card = cards[i - 1];
+            if (!card.classList.contains("empty")) {
+                card.classList.add("back");
+                card.classList.remove("show");
+    
+                NSJ.GetDeepestChild("#DrawPile .handle").appendChild(card);
+            }
+        }
+
+        let hands = document.querySelectorAll("#Hands .hand");
+        hands.forEach(hand => {
+            if (!hand.classList.contains("base")){
+                hand.remove();
+            }
+        });
+    }
+
+    Draw = (count = 3) => {
+        let newHandFragment = document.importNode(this._handTemplate.content, true);
+        let newHand = newHandFragment.querySelector(".hand");
+
+        for(let i = 0; i < count; i++) {
+            let card = NSJ.GetDeepestChild("#DrawPile .card");
+        
+            if (!card.classList.contains("empty")) {
+                card.classList.remove("back");
+                card.classList.add("show");
+
+                newHand.appendChild(card);
+            }
+            else if (i == 0) {
+                this._resetDeck();
+                i = count;
+            }
+        }
+
+        if (newHand.children.length > 0) {
+            NSJ.GetDeepestChild("#Hands .hand").appendChild(newHandFragment);
+        }
+    }
+
+    _pickUpCard = function(event) {
+        //if (stack.classList.contains("back")) return false;
+
+
+        this._dragBox.appendChild(stack);
+
+        //stack.classList.add("drag");
+        
+
+        //document.getElementById("Gameboard").appendChild(stack);
+        //document.body.appendChild(stack);
+
+        // let moveAt = (pageX, pageY) => {
+        //     stack.style.left = (pageX - shiftX) + "px";
+        //     stack.style.top = (pageY - shiftY) + "px";
+        // }
+
+        // moveAt(event.pageX, event.pageY);
+
+        function dragCard (){
+        }
+
+        // function releaseCard() {
+        //     //document.removeEventListener('mousemove', dragCard, false);
+        //     stack.onmouseup = null;
+        //     if (_validateCardRelease()) {
+
+        //     }
+        //     else {
+        //         origin.appendChild(stack);
+        //     }
+        //     stack.classList.remove("drag");
+        // }
+
+        //document.addEventListener('mousemove', dragCard, false);
+
+        //stack.onmouseup = releaseCard.bind(this);
+    }
+
+
+
+    _init() {
+        // Draw Functions
+        this._drawPile.addEventListener("click", () => {
+            this.Draw();
+        });
+
+        // Drag Functions
+        let shiftX = 0, 
+            shiftY = 0,
+            zone = "";
+        // this._dragBox.ondragstart = () => {
+        //     return false;
+        // }
+        function moveDragBox (event) {
+
+            if (this._dragBox.childNodes.length > 0) {
+                this._dragBox.hidden = true;
+                let target = document.elementFromPoint(event.clientX, event.clientY);
+                if (!target) return;
+                zone = NSJ.GetParentID(target);
+                this._dragBox.hidden = false;
+
+                if (!zone) return;
+
+            }
+
+            this._dragBox.style.left = `${event.pageX - shiftX}px`;
+            this._dragBox.style.top = `${event.pageY - shiftY}px`;
+        }
+
+        let setEvents = (el) => {
+            let origin, stack;
+
+            el.onmousedown = (event) => {
+                let elementRect  = el.getBoundingClientRect();
+                shiftX = event.clientX - elementRect.left;
+                shiftY = event.clientY - elementRect.top;
+                moveDragBox.bind(this)(event);
+                stack = event.target.parentElement;
+                origin = stack.parentElement;
+                this._dragBox.appendChild(stack);
+            }
+
+            el.onmouseup = (event) => {
+                if (this._dragBox.childNodes.length > 0) {
+                    if (validateCardRelease(stack)) {
+                        NSJ.GetDeepestChild(`#${zone} .handle`).appendChild(stack);
+                    }
+                    else {
+                        origin.appendChild(stack);
+                    }
+                }
+            }
+        }
+
+        let validateCardRelease = (el) => {
+            let suit = el.getAttribute("suit");
+            let val = el.getAttribute("card-value");
+            let stackHeight = el.querySelectorAll(".handle").length;
+            // if Zone matches is in correct run, can only push stacks of one
+            if (zone === suit && stackHeight === 1) {
+                let runEnd = NSJ.GetDeepestChild(`#${zone} .card`);
+                let endValue = Number.parseInt(runEnd.getAttribute("card-value"));
+                if (endValue === val - 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        document.onmousemove = moveDragBox.bind(this);
+
+        document.querySelectorAll(".card.show .handle").forEach(card => {
+            setEvents(card);
+        });
+    }
+
+    constructor (options) {
+        this._deck = new Deck();
+        this._layout();
+        this._init();
+    }
+}
+
+const test = new KlondikeGameBoard();
+
+//test.init();
