03c66d37c5610a368afcd3a3df8dabed83d62791
Chapel Studios <rev.jake@gmail.com>
2020-05-25 23:38:57 -0700

    added image assets to cards

--- Areas/Games/solitaireTest/card-deck.js
+++ Areas/Games/solitaireTest/card-deck.js
@@ -2,13 +2,32 @@
 
 class Card {
     static _template = document.getElementById("CardTemplate");
+    static _suitImageTemplate = document.getElementById("SuitImageTemplate");
+    //static _imageBasePath = "/wwwroot/assets/Images/Games/Cards/";
+    static _imageBasePath = "/Images/";
 
     static Create (suit, value) {
         let displayValue = value;
+        let centerCount = value;
+        // ToDo: change this to dependancy injection...
+        let centerImgSrc = this._imageBasePath + `${suit.name}`;
+
         if (value == 1) displayValue = "A";
-        else if (value == 11) displayValue = "J";
-        else if (value == 12) displayValue = "Q";
-        else if (value == 13) displayValue = "K";
+        else if (value == 11) {
+            displayValue = "J";
+            centerCount = 1;
+            centerImgSrc = centerImgSrc + `_Jack.png`;
+        }
+        else if (value == 12) {
+            displayValue = "Q";
+            centerCount = 1;
+            centerImgSrc = centerImgSrc + `_Queen.png`;
+        }
+        else if (value == 13) {
+            displayValue = "K";
+            centerCount = 1;
+            centerImgSrc = centerImgSrc + `_King.png`;
+        }
 
         let clone =  document.importNode(this._template.content, true);
         
@@ -22,16 +41,32 @@ class Card {
         let isRedAttr = document.createAttribute("is-red");
         isRedAttr.value = suit.isRed;
         card.setAttributeNode(isRedAttr);
-        
+
+        // Add Corner Images
+        clone.querySelectorAll(".top img, .bottom img").forEach((img) => {
+            img.src = imgSrc;
+        });
+
+        // Add Center Images
+        [...Array(centerCount)].forEach(i => {
+            let imgClone =  document.importNode(this._suitImageTemplate.content, true);
+            let img = imgClone.querySelector("*");
+            let center = clone.querySelector(".center");
+            if (centerCount === 1 && value != 1) {
+                img.style.backgroundImage = `url(${centerImgSrc})`;
+                center.classList.add("face");
+            }
+            let centerCountAttr = document.createAttribute("count");
+            centerCountAttr.value = centerCount;
+            center.setAttributeNode(centerCountAttr);
+            center.appendChild(imgClone);
+        });
+
         let valDivs = clone.querySelectorAll(".value");
         valDivs.forEach(div => {
             div.append(displayValue)
         });
         
-        let suits = clone.querySelectorAll(".suit");
-        suits.forEach(div => {
-            div.append(suit.name);
-        });
         
         return clone;
     }
