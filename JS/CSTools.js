216bc59ad8e94f57221cd8b57b65e44963b31349
Chapel Studios <rev.jake@gmail.com>
2020-05-29 10:43:14 -0700

    imported additional CSTools

--- JS/CSTools.js
+++ JS/CSTools.js
@@ -19,3 +19,24 @@ CSTools.GrammerHelper = {
         return input + this.GetCordialSuffix(input);
     }
 }
+
+CSTools.Math = {
+    Random: (max, min = 0) => {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+}
+
+CSTools.HTMLHelper = {
+    GetDeepestChild: (selector) => {
+        let nodes = document.querySelectorAll(selector);
+        if (!nodes.length) return false;
+        return nodes[nodes.length - 1];
+    }
+    , GetParentID: (node) => {
+        if (node.id != "") {
+            return node.id;
+        }
+        if (!node.parentElement) return false;
+        return CSTools.GetParentID(node.parentElement);
+    }
+}
