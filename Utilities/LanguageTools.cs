984abf119684c07106f2b4ac86914d260fc9b680
Chapel Studios <rev.jake@gmail.com>
2020-09-13 08:31:41 -0700

    Added JS Tool to handle capitalization for first letter of a string

---
+++ Utilities/LanguageTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapelStudiosWWW.Utilities
+{
+    public static class LanguageTools
+    {
+        public static string CapitalizeFirst (string original)
+        {
+            if (string.IsNullOrEmpty(original)) return string.Empty;
+            char[] chars = original.ToCharArray();
+            chars[0] = char.ToUpper(chars[0]);
+            return new string(chars);
+        }
+    }
+}
