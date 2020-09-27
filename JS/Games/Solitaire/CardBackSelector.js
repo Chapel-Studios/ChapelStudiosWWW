712b387f894009626cfea369c92ac295a8ba3a2a
Chapel Studios <rev.jake@gmail.com>
2020-09-27 12:30:23 -0700

    modal vm for cardback selector

---
+++ JS/Games/Solitaire/CardBackSelector.js
@@ -0,0 +1,79 @@
+﻿class CardBackSelector {
+    OGImage;
+    currentImage;
+    updateCardBack;
+    Modal = document.getElementById("Modal-CBS");
+    GameBoard = document.getElementById("Gameboard");
+
+    constructor(startingImage, cardBackOptions, changeCardBackCallback) {
+        this.OGImage = startingImage;
+        this.currentImage = startingImage;
+        this.updateCardBack = changeCardBackCallback;
+
+        this.setInitialBindings();
+    }
+
+    setInitialBindings = () => {
+        let optionNodes = document.querySelectorAll(".card-options .playing-card");
+        optionNodes.forEach(node => {
+            node.onclick = (event) => {
+                document.querySelector(".card-options .playing-card.active").classList.remove("active");
+                event.target.classList.add("active");
+                this.currentImage = event.target.attributes["option"].value;
+            }
+        });
+
+        document.getElementById("Accept-CBS").onclick = (event) => {
+            this.selectImage();
+            this.Close();
+        }
+
+        document.getElementById("Cancel-CBS").onclick = (event) => {
+            this.Reset();
+        }
+
+        document.getElementById("Open-CBS").onclick = (event) => {
+            this.Open();
+        }
+
+        this.Modal.onclick = (event) => {
+            this.Reset();
+        }
+
+        this.Modal.querySelector(".exit").onclick = (event) => {
+            this.Reset();
+        }
+
+        this.Modal.querySelector(".content").onclick = (event) => {
+            event.stopPropagation();
+        }
+
+    }
+
+    Reset = () => {
+        this.resetActiveImage();
+        this.Close();
+    }
+
+    resetActiveImage = () => {
+        this.Modal.querySelector(`.${this.currentImage}`).classList.remove("active");
+        this.Modal.querySelector(`.${this.OGImage}`).classList.add("active");
+        this.currentImage = this.OGImage;
+    }
+
+    selectImage = () => {
+        this.updateCardBack(this.currentImage);
+        // ToDo: add this to cookie and DB for registered users
+    }
+
+    Open = () => {
+        this.OGImage = this.currentImage;
+        this.GameBoard.classList.add("bind-position");
+        this.Modal.classList.add("open");
+    }
+
+    Close = () => {
+        this.Modal.classList.remove("open");
+        this.GameBoard.classList.remove("bind-position");
+    }
+}
