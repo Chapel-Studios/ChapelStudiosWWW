695827ba7099520d9161211690048efe96342fcf
Chapel Studios <rev.jake@gmail.com>
2020-10-08 14:00:13 -0700

    Moved layout creation to view file to reduce JS load on browsers - this didn't resolve the FF perf issue though :(

---
+++ Areas/Games/Models/CreateSolitaireSectionOptionsSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapelStudiosWWW.Areas.Games.Models
+{
+    public class CreateSolitaireSectionOptionsSet
+    {
+        public string Suit { get; set; }
+        public bool IsCenterNeeded { get; set; }
+
+        public CreateSolitaireSectionOptionsSet(bool isCenterNeeded, string suit = "")
+        {
+            this.IsCenterNeeded = isCenterNeeded;
+            this.Suit = suit;
+        }
+    }
+}
